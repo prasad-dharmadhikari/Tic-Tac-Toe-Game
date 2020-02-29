3249d0e27e3b325697a3b0298db23a7122d54721
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-29 17:38:21 +0530

    ADDED:Make a toss and decide who plays first

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -6,16 +6,25 @@ declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
 echo "Welcome to tic tac toe game"
 
-echo "${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
-echo "-----------"
-echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
-echo "-----------"
-echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+echo "		${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+echo "		-----------"
+echo "		${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+echo "		-----------"
+echo "		${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
 
 letterCheck=$((RANDOM%2))
 case $letterCheck in 
 	0) echo "You have assigned : "$O
 	;;
 	1) echo "You have assigned : "$X
+	;;
+esac
+
+
+firstTurn=$((RANDOM%2))
+case $firstTurn in
+	0) echo "User plays first"
+	;;
+	1) echo "Computer plays first"
 	;;
 esac
