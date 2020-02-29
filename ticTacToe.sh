5178401ca415437b499c6adc50478fda21227f58
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-29 17:12:07 +0530

    ADDED: Assign the letter to the player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+
+X="X"
+O="O"
 declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
 echo "Welcome to tic tac toe game"
@@ -8,3 +11,11 @@ echo "-----------"
 echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
 echo "-----------"
 echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+
+letterCheck=$((RANDOM%2))
+case $letterCheck in 
+	0) echo "You have assigned : "$O
+	;;
+	1) echo "You have assigned : "$X
+	;;
+esac
